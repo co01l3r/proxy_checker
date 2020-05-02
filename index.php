535a2f11242c9c92757eb58dd8b99f6336aa5c83
theRealCooller <jakublohnicky@hotmail.com>
2020-05-02 16:55:47 +0200

    index update

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ use theRealCooller\ProxyChecker;
 require __DIR__ . '/vendor/autoload.php';
 
 $urlResponse = new ProxyChecker\UrlResponse('https://www.csfd.cz/', new ProxyChecker\RandomUserAgent(), 3);
-$textFileOutput = new ProxyChecker\TextFileOutput('result.txt');
-$massOutput = new ProxyChecker\MassOutput($textFileOutput, new ProxyChecker\StdOutput());
+$textFilePositiveOutput = new ProxyChecker\TextFileOutput('positive-results.txt');
+$massOutput = new ProxyChecker\MassOutput($textFilePositiveOutput, new ProxyChecker\StdOutput());
+$nullOutput = new ProxyChecker\DevNullOutput();
+$textFileNegativeOutput = new ProxyChecker\TextFileOutput('negative-results.txt');
 
-
-$proxy = new ProxyChecker\ProxyCheck('/var/www/ProxyChecker/proxy', $massOutput, $urlResponse);
+$proxy = new ProxyChecker\ProxyCheck('/var/www/ProxyChecker/proxyx', $massOutput, $nullOutput, $urlResponse);
 $proxy->check();
