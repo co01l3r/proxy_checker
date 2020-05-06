1593a0b94bbfccbe223447c116cda381a8a8f3f7
theRealCooller <jakublohnicky@hotmail.com>
2020-05-06 17:55:34 +0200

    index update

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
 declare(strict_types=1);
 
-
 use theRealCooller\ProxyChecker;
-
 require __DIR__ . '/vendor/autoload.php';
 
-$urlResponse = new ProxyChecker\UrlResponse('https://www.csfd.cz/', new ProxyChecker\RandomUserAgent(), 3);
-$textFilePositiveOutput = new ProxyChecker\TextFileOutput('positive-results.txt');
+//change these as you see fit:
+$resource = '/var/www/ProxyChecker/proxyz';
+$urlToTest = 'https://www.csfd.cz/';
+$positiveResultFileName = 'positive-result.txt';
+$negativeResultFileName = 'negative-results.txt';
+
+
+$urlResponse = new ProxyChecker\UrlResponse($urlToTest, new ProxyChecker\RandomUserAgent(), 3);
+$textFilePositiveOutput = new ProxyChecker\TextFileOutput($positiveResultFileName);
 $massOutput = new ProxyChecker\MassOutput($textFilePositiveOutput, new ProxyChecker\StdOutput());
 $nullOutput = new ProxyChecker\DevNullOutput();
-$textFileNegativeOutput = new ProxyChecker\TextFileOutput('negative-results.txt');
-$Results = new ProxyChecker\FinalProgress('positive-results.txt', 'negative-results.txt', '/var/www/ProxyChecker/proxyxy');
+$textFileNegativeOutput = new ProxyChecker\TextFileOutput($negativeResultFileName);
+$Results = new ProxyChecker\FinalProgress($positiveResultFileName, $negativeResultFileName, $resource);
 
-$proxy = new ProxyChecker\ProxyCheck('/var/www/ProxyChecker/proxyxy', $massOutput, $nullOutput, $urlResponse, $Results);
+$proxy = new ProxyChecker\ProxyCheck($resource, $massOutput, $nullOutput, $urlResponse, $Results);
 $proxy->check();
-
